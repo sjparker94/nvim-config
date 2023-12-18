b207889364856d06c20c1cfb95b22f882408dfee
Simon Parker <simon.parker1@homegroup.org.uk>
2023-12-18 14:26:41 +0000

    feat: update colour scheme to gruvbox

--- lua/sjparker94/core/colorscheme.lua
+++ lua/sjparker94/core/colorscheme.lua
@@ -1,8 +1,7 @@
 -- set colorscheme to nightfly with protected call
 -- in case it isn't installed
-local status, _ = pcall(vim.cmd, "colorscheme onedark")
+local status, _ = pcall(vim.cmd, "colorscheme gruvbox")
 if not status then
 	print("Colorscheme not found!") -- print error if colorscheme not installed
 	return
 end
-
