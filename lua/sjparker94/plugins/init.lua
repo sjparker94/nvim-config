c74ab2f4f344098bb74e1314fe265ee36d84f754
Simon Parker <simon.parker1@homegroup.org.uk>
2023-12-20 11:22:45 +0000

    feat: add no custom config plugins into plugins/init.lua

---
+++ lua/sjparker94/plugins/init.lua
@@ -0,0 +1,7 @@
+return {
+	"nvim-lua/plenary.nvim", -- lua functions that many plugins use
+	"christoomey/vim-tmux-navigator", -- tmux & split window navigation
+	"vim-scripts/ReplaceWithRegister",
+	"tpope/vim-fugitive",
+	"romgrk/barbar.nvim",
+}
