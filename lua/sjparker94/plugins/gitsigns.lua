d223f4e4f421cbf92b2c960500031a05f57696e5
Simon Parker <simon.parker1@homegroup.org.uk>
2024-03-11 17:03:25 +0000

    feat: add git blame keymapping

--- lua/sjparker94/plugins/gitsigns.lua
+++ lua/sjparker94/plugins/gitsigns.lua
@@ -1,5 +1,20 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	event = { "BufReadPre", "BufNewFile" },
-	config = true,
+	config = function()
+		local gitsigns = require("gitsigns")
+
+		gitsigns.setup({
+			on_attach = function()
+				local keymap = vim.keymap
+
+				keymap.set(
+					"n",
+					"<leader>gb",
+					"<cmd>Gitsigns toggle_current_line_blame<CR>",
+					{ desc = "Toggle current line git blame" }
+				) -- restore last workspace session for current directory
+			end,
+		})
+	end,
 }
