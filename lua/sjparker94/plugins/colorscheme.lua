3ee5bdcb95fe7466c5a9459bc85e964f94c2aa07
Simon Parker <simon.parker1@homegroup.org.uk>
2024-04-29 10:45:49 +0100

    feat: update to catppuccin color scheme

--- lua/sjparker94/plugins/colorscheme.lua
+++ lua/sjparker94/plugins/colorscheme.lua
@@ -1,8 +1,19 @@
 return {
-	"ellisonleao/gruvbox.nvim",
+	"catppuccin/nvim",
 	priority = 1000,
 	config = function()
-		require("gruvbox").setup()
-		vim.cmd("colorscheme gruvbox")
+		require("catppuccin").setup({
+			flavour = "mocha",
+			integrations = {},
+		})
+		vim.cmd("colorscheme catppuccin")
 	end,
 }
+-- return {
+-- 	"ellisonleao/gruvbox.nvim",
+-- 	priority = 1000,
+-- 	config = function()
+-- 		require("gruvbox").setup()
+-- 		vim.cmd("colorscheme gruvbox")
+-- 	end,
+-- }
--- lua/sjparker94/plugins/lualine.lua
+++ lua/sjparker94/plugins/lualine.lua
@@ -8,7 +8,7 @@ return {
 		-- configure lualine with modified theme
 		lualine.setup({
 			options = {
-				theme = "gruvbox",
+				theme = "catppuccin",
 			},
 		})
 	end,
